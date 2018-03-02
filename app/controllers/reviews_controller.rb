9f1741b86de8f2c763b267542df052c8f89d38e0
Martin-Alexander <martingianna@gmail.com>
2018-03-02 10:53:07 -0500

    the rest of the app

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+
+    @review.save
+
+    # if @review.save
+    #   redirect_to restaurant_path(@restaurant)
+    # else
+    #   render "restaurants/show" 
+    # end
+
+    # render reviews/create.js.erb
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :restaurants, only: [ :index, :show, :destroy ] do
+    resources :reviews, only: :create
+  end
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
